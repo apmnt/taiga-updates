efbfbf99401e1fc83e6430ad0a5090df52f0e22a
apmnt <aapo.montin@student.oulu.fi>
2025-02-08 18:00:19 +0200

    Update website title

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ products = data["result"]["serverData"]["data"]["collection"]["products"]["edges
 
 def render_header(change_view_href):
     return (
+        Title("Taiga Stock Status"),
         Socials(
             title="Taiga Takahashi stock status",
             site_name="Vercel",
